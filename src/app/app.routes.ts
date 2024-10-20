7ecdea837123fbcd912f5305ba8e9f3bcc248057
van_da <iivnnn123@gmail.com>
2024-10-20 14:14:19 -0500

    add path projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/presentation/home/home.component';
 
 export const routes: Routes = [{
     path: '',
     redirectTo: 'van',
     pathMatch: 'full'
+},{
+    path: 'me',
+    component: HomeComponent
 },{
     path: 'van',
     loadChildren: () => import('./modules/presentation/presentation.module').then(m => m.PresentationModule)
