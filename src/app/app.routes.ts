bdf07ee0472ecc261b2c7ec9800b61b87b98b69f
van_da <iivnnn123@gmail.com>
2024-10-21 09:42:55 -0500

    chage route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/presentation/home/home.component';
 
 export const routes: Routes = [{
-    path: '',
-    redirectTo: 'van',
-    pathMatch: 'full'
+ path: '',   
+ loadComponent: () => import("./modules/presentation/home/home.component").then(p => p.HomeComponent) 
 },{
     path: 'me',
-    component: HomeComponent
+    redirectTo: 'van',
+    pathMatch: 'full'
 },{
     path: 'van',
     loadChildren: () => import('./modules/presentation/presentation.module').then(m => m.PresentationModule)
